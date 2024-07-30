0db90590751ad9e9722fbe537d15b323a8f878fa
Corey Henderson <corey@recordsetter.com>
2024-07-30 14:58:24 +0200

    fix strapi cloud maybe

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,7 +8,7 @@ module.exports = [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com', 'media.strapiapp.com'],
           'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
           upgradeInsecureRequests: null,
         },
